0fdd424f5364396334e879d7bd91982b2d803417
jmsalexmech054 <jmsalexmech054@gmail.com>
2023-04-19 12:58:29 +0530

    newly added kavin repo

---
+++ kavin/src/kavin_pack1/kavinclass_1.java
@@ -0,0 +1,12 @@
+package kavin_pack1;
+
+public class kavinclass_1 {
+
+public static void main(String[] args) {
+		
+		for(int i=17;i<=24;i++){
+			System.out.println(i);
+		}
+		System.out.print("hello kavin");
+	}
+}
