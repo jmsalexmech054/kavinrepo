6caea0c9807226458a382420797ae844ba61d3e7
kavinkumar94 <124357834+kavinkumar94@users.noreply.github.com>
2023-04-19 13:11:17 +0530

    Update kavinclass_1.java

--- kavin/src/kavin_pack1/kavinclass_1.java
+++ kavin/src/kavin_pack1/kavinclass_1.java
@@ -4,9 +4,9 @@ public class kavinclass_1 {
 
 public static void main(String[] args) {
 		
-		for(int i=17;i<=24;i++){
+		for(int i=17;i<=215;i++){
 			System.out.println(i);
 		}
-		System.out.print("hello kavin");
+		System.out.print("hello kavinkumar hi da ");
 	}
 }
